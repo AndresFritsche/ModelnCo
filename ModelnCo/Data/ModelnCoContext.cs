270df87ae0b0883df850dacdda199e3c54a76ba2
andresf0090@gmail.com <andresf0090@gmail.com>
2025-04-23 12:23:25 -0400

    Add project files.

---
+++ ModelnCo/Data/ModelnCoContext.cs
@@ -0,0 +1,9 @@
+using System;
+using Microsoft.EntityFrameworkCore;
+
+namespace ModelnCoAPI.Data;
+
+public class ModelnCoContext : DbContext
+{
+
+}
---
+++ ModelnCo/Entities/Product.cs
@@ -0,0 +1,25 @@
+
+namespace ModelnCoAPI.Entities;
+
+public class Product
+{
+    public int Id { get; set; }
+
+    public required string Name { get; set; }
+
+    public required string Brand  { get; set; }
+
+    public int Weight { get; set; }
+
+    public string? Dimension { get; set; }
+
+    public string? Category { get; set; }
+
+    public long Price { get; set; }
+
+    public long PictureUrl { get; set; }
+
+    public string? Description { get; set; }
+    
+    public int QtyInStock  { get; set; }
+}
